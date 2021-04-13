15d32887fa2f0cdcb40d3eeb41ea827da4b0bd46
Joshua Ellis <joshpoodle42@gmail.com>
2021-04-13 16:27:14 +0000

    Next Bootstrap Website

---
+++ components/SideBar.js
@@ -0,0 +1,10 @@
+import { Container } from 'react-bootstrap'
+import styles from "../styles/Home.module.css";
+
+export default function NavBar () {
+    return (
+        <Container style={{ position: "relative" ,height: "100%", width: "10%", backgroundColor: "#000000", background: "#000000"}}>
+            
+        </Container> 
+    )
+}
